6b58c843c6d99b481b6c19bba5c19963ddb64c43
Edward Cheadle <edward.cheadle@cambiahealth.com>
2018-04-14 22:03:39 -0600

    Added web server

--- main.tf
+++ main.tf
@@ -2,7 +2,13 @@ resource "aws_instance" "example" {
   ami           = "ami-d874e0a0"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+      #!/bin/bash
+      echo "Hello, World" > index.html
+      nohup busybox httpd -f -p 8080 &
+      EOF
+
   tags {
-    Name = "terraform-example"
+    Name = "terraform-example-web"
   }
 }
