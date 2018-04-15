066f828e5f013d906e2806058f679db85291a2f2
Edward Cheadle <edward.cheadle@cambiahealth.com>
2018-04-14 21:57:36 -0600

    Added tag to instance and items to .gitignore

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
-
 resource "aws_instance" "example" {
-ami = "ami-d874e0a0"
-instance_type = "t2.micro"
+  ami           = "ami-d874e0a0"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "terraform-example"
+  }
 }
